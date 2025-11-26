d6c362a0b4cb8333797a27275694ed456998cdd4
Ishika Agarwal <ishikaagarwal2211@gmail.com>
2025-11-26 17:23:11 +0530

    project using web api

--- Projects HTML_CSS_JS/Random Cats/script.js
+++ Projects HTML_CSS_JS/Random Cats/script.js
@@ -1,25 +1,43 @@
-let url = "https://api.thecatapi.com/v1/images/search";
-let img = document.querySelector("#catImage");
-fetch(url)
-  .then((response) => {
-    console.log(response);
-     response.json().then((readableData) => {
-      img.src = readableData[0].url;
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+let button = document.querySelector("button");
+let title = document.querySelector("h1");
+let p = document.createElement("p");
+let img = document.createElement("img");
+img.classList.add("catImage");
 
-  let fact=document.querySelector("#catFact");
-  let url2 = "https://catfact.ninja/fact?max_length=140";
-  fetch(url2)
+let url = "https://api.thecatapi.com/v1/images/search";
+let getCatImage = () => {
+  fetch(url)
     .then((response) => {
       console.log(response);
       response.json().then((readableData) => {
-          fact.innerText=readableData.fact;
+        let imgUrl = readableData[0].url;
+        console.log(imgUrl);
+        img.src = imgUrl;
+        button.after(img);
       });
     })
     .catch((error) => {
       console.log(error);
     });
+};
+
+let getCatFact = () => {
+  let fact = document.querySelector("#catFact");
+  let url2 = "https://catfact.ninja/fact?max_length=140";
+  fetch(url2)
+    .then((response) => {
+      console.log(response);
+      response.json().then((readableData) => {
+          p.innerText = readableData.fact;
+            img.after(p);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+button.addEventListener("click", () => {
+  getCatImage();
+  getCatFact();
+});
