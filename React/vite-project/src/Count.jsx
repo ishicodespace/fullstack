629c6d6e69dab2ce33383e0122a613af6c0f8151
Ishika Agarwal <ishikaagarwal2211@gmail.com>
2025-12-05 17:23:19 +0530

    states in react

--- React/vite-project/src/Count.jsx
+++ React/vite-project/src/Count.jsx
@@ -1,15 +1,21 @@
+// any state update triggers a re-render of that component.
 import { useState } from "react";
+
 export default function Count() {
-  let [count, setCount] = useState(0);
+  //rerendered every time state changes
+
+  let [count, setCount] = useState(0); //array destructuring because useState returns an array of two elements
+  console.log("component is rendered");
+  console.log(count);
 
   let Increment = () => {
-    setCount(count + 1);
-    console.log(count);
+    setCount(count + 1); // schedules an update
+    console.log("inside increment " + count); // shows the old value due to closure
   };
   return (
     <div>
       <h3>Count ={count}</h3>
-      <button onClick={Increment}></button>
+      <button onClick={Increment}>Increment</button>
     </div>
   );
 }
