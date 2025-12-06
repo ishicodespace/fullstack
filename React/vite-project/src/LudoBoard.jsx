23610c7db0d19ceaf7d2f0cbb9aba42485613817
Ishika Agarwal <ishikaagarwal2211@gmail.com>
2025-12-06 23:56:11 +0530

    objects and state

--- React/vite-project/src/LudoBoard.jsx
+++ React/vite-project/src/LudoBoard.jsx
@@ -1,22 +1,56 @@
 import { use, useState } from "react";
-
 export default function LudoBoard() {
-    let [moves, setMoves] = useState({ blue: 0, red: 0, yellow: 0, green: 0 });
-    let updateBlue = () => {
-        setMoves(moves.blue + 1);
-        console.log(moves.blue);
-    }
-    return (
-        <div>
-            <p>Game Begins</p>
-            <div className="Board">
-                <p>Blue</p>
-                <button>{moves.blue}</button>
-                <p>Red</p>
-                <button>{moves.red}</button>   <p>Yellow</p>
-                <button>{moves.yellow}</button>   <p>Green</p>
-                <button>{moves.green}</button>
-            </div>
-        </div>
-    )
+  let [moves, setMoves] = useState({ blue: 0, yellow: 0, green: 0, red: 0 });
+
+  function updateBlue() {
+    //since blue new value depends on old value use callback form of setState
+    setMoves((prevMoves) => ({
+      ...prevMoves, //spread so it will re render and a change in object will be detected, new object is created
+      blue: prevMoves.blue + 1,
+    }));
+  }
+
+  function updateRed() {
+    setMoves((prevMoves) => ({
+      ...prevMoves, //spread operator to retain other colors' moves
+      red: prevMoves.red + 1,
+    }));
+  }
+
+  function updateYellow() {
+    setMoves((prevMoves) => ({
+      ...prevMoves, //spread operator to retain other colors' moves
+      yellow: prevMoves.yellow + 1,
+    }));
+  }
+
+  function updateGreen() {
+    setMoves((prevMoves) => ({
+      ...prevMoves, //spread operator to retain other colors' moves
+      green: prevMoves.green + 1,
+    }));
+  }
+
+  return (
+    <div>
+      <p>Game Begins</p>
+      <div className="board"></div>
+      <p>Blue Moves: {moves.blue} </p>
+      <button style={{ backgroundColor: "blue" }} onClick={updateBlue}>
+        +1
+      </button>
+      <p>Yellow Moves: {moves.yellow} </p>
+      <button style={{ backgroundColor: "yellow" }} onClick={updateYellow}>
+        +1
+      </button>
+      <p>Green Moves: {moves.green} </p>
+      <button style={{ backgroundColor: "green" }} onClick={updateGreen}>
+        +1
+      </button>
+      <p>Red Moves: {moves.red} </p>
+      <button style={{ backgroundColor: "red" }} onClick={updateRed}>
+        +1
+      </button>
+    </div>
+  );
 }
