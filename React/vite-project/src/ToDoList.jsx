3607ca76cf14226f749d5d21e7eac02f0f08b9b6
Ishika Agarwal <ishikaagarwal2211@gmail.com>
2025-12-12 23:59:59 +0530

    todo in react

--- React/vite-project/src/ToDoList.jsx
+++ React/vite-project/src/ToDoList.jsx
@@ -3,14 +3,23 @@ import { useState } from "react";
 export default function ToDoList() {
   let [tasks, setTasks] = useState(["eat"]);
   let [newToDo, setNewToDo] = useState("");
-
-  
+  function addTask() {
+    let updatedTasks = [...tasks, newToDo];
+    setTasks(updatedTasks);
+    setNewToDo("");
+  }
 
   return (
     <div>
       <h1>TO DO LIST</h1>
-      <input type="text" placeholder="enter task to add" />
-      <button>Add</button>
+      <input
+        type="text"
+        placeholder="enter task to add"zz
+        value={newToDo}
+        onChange={(e) => setNewToDo(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add</button>
       <ul>
         {tasks.map((task) => {
           return <li>{task}</li>;
